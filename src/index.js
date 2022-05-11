5bd4016509e428997a7a7997c58462d65b8edd80
Adolfo Chafardett <adolfo1997a@gmail.com>
2022-05-11 18:24:44 -0400

    Finally we get the sku by rows only in the first colum of our test xlsx

--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
-require('dotenv').config()
-const exceljs = require('exceljs')
+require('dotenv').config();
+const exceljs = require('exceljs');
 
-const getWorkBook = (path) => {
-    return Promise((resolve, reject) => {
-        new exceljs.Workbook().xlsx.readFile(path).then((value) => resolve(value)).catch((reason) => reject(reason))
+(() => {
+    new exceljs.Workbook().xlsx.readFile(process.env.XLSX_FILE_PATH).then((workbook) => {
+        const rows = workbook.getWorksheet(1).getRows(1, workbook.getWorksheet(1).rowCount)
+        rows.forEach((row) => {
+            let sku = row.values.splice(1,1).shift()
+            // if(sku) // Requests to VTEX
+        })
+    }).catch((reason) => {
+        console.log(reason)
     })
-}
-
-const selectWorksheet = (workbook, numberWorkSheet) => {
-    return workbook.getWorksheet(numberWorkSheet)
-}
-
-const getRows = (worksheet, start, lengthWorksheet) => {
-    return worksheet.getRows(start, lengthWorksheet)
-}
-
-exports.module = {
-    getWorkBook,
-    selectWorksheet,
-    getRows
-}
-
-
-
-
-// '/home/adolfo/work/acid/cenco/resynchro/Cruce Prod No Existe VTEX (777).xlsx'
+})();
